6f84eecfc15dbcfbea26263f4e9cfa7d96568afb
Percy <percival.ngan@gmail.com>
2019-09-23 23:14:04 -0500

    Completed easy challenge

--- Challenge1.playground/Contents.swift
+++ Challenge1.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var str = "Hello, playground"
+func concat(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+	let combined = arr1 + arr2
+	print(combined)
+
+	return(combined)
+}
+
+concat([1, 2, 3], [4, 5, 6])
