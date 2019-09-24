1c8815a7db840026de37911084240159d4220d43
Percy <percival.ngan@gmail.com>
2019-09-24 01:21:42 -0500

    More 1st day easy challenge

--- Challenge1.playground/Contents.swift
+++ Challenge1.playground/Contents.swift
@@ -1,10 +1,63 @@
 import UIKit
 
-func concat(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
-	let combined = arr1 + arr2
-	print(combined)
+//func concat(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+//	let combined = arr1 + arr2
+//	print(combined)
+//
+//	return(combined)
+//}
+//
+//concat([1, 2, 3], [4, 5, 6])
 
-	return(combined)
+//func addition(number: Int) -> Int {
+//
+//	let result = number + 1
+//
+//	print(result)
+//
+//	return result
+//}
+//
+//addition(number: 5)
+
+//func lessThanOrEqualTo(num: Int) -> Bool {
+//
+//	if num <= 0 {
+//		return true
+//	} else {
+//		return false
+//	}
+//}
+//
+//lessThanOrEqualTo(num: -1)
+
+//func check(arr: [Int], num: Int) -> Bool {
+//
+//	if arr.contains(num) {
+//		return true
+//	} else {
+//		return false
+//	}
+//}
+//
+//check(arr: [11,12,1,22,1,3], num: 2)
+
+//func reverse(_ arr: [Int]) -> [Int] {
+//
+//
+//
+//	return arr.reversed()
+//}
+//
+//reverse([2,4,6,7,8,9])
+
+func getLastItem(_ arr: [Any]) -> Any {
+
+	let reversedArray = arr.last
+	print (reversedArray as Any)
+
+	return reversedArray
+
 }
 
-concat([1, 2, 3], [4, 5, 6])
+getLastItem([3, "peter", "eweww", "last item"])
